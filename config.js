b85e3736ecdbf3e81a4abe4aa763d9274cb9ceb9
mobatv <53135012+mobatv@users.noreply.github.com>
2026-08-28 00:16:14 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+// config.js
+window.APP_CONFIG = {
+    firebaseConfig: {
+        apiKey: "AIzaSyCqsv8xo0KsAyM-TYIE58jqVvS2JPAeMk",
+        authDomain: "ngochao-90782.firebaseapp.com",
+        databaseURL: "https://ngochao-90782-default-rtdb.firebaseio.com",
+        projectId: "ngochao-90782",
+        storageBucket: "ngochao-90782.firebasestorage.app",
+        messagingSenderId: "609549450140",
+        appId: "1:609549450140:web:4abf6b9a681805ff412659",
+        measurementId: "G-T8YGX6ZW0D"
+    },
+    defaultAdminPasswordHash: "240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9"
+};
