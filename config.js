25fd2845cfc974ab86edb6e2048c9f997515b1fd
mobatv <53135012+mobatv@users.noreply.github.com>
2026-08-28 00:31:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 // config.js
 window.APP_CONFIG = {
-    firebaseConfig: {
-        apiKey: "AIzaSyCqsv8xo0KsAyM-TYIE58jqVvS2JPAeMk",
-        authDomain: "ngochao-90782.firebaseapp.com",
-        databaseURL: "https://ngochao-90782-default-rtdb.firebaseio.com",
-        projectId: "ngochao-90782",
-        storageBucket: "ngochao-90782.firebasestorage.app",
-        messagingSenderId: "609549450140",
-        appId: "1:609549450140:web:4abf6b9a681805ff412659",
-        measurementId: "G-T8YGX6ZW0D"
-    },
+   const firebaseConfig = {
+  apiKey: "AIzaSyB82JABeqIWdQm1842qnnWIvuaeyRNgyog",
+  authDomain: "vpsvn-8ebd6.firebaseapp.com",
+  databaseURL: "https://vpsvn-8ebd6-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "vpsvn-8ebd6",
+  storageBucket: "vpsvn-8ebd6.firebasestorage.app",
+  messagingSenderId: "947319716277",
+  appId: "1:947319716277:web:5ed25aae69ec7f5e0501fc",
+  measurementId: "G-RWKZQDVVR5"
+};
     defaultAdminPasswordHash: "240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9"
 };
